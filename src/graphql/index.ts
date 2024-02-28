5096d7f5950bdebd13108cfcb0ece19828e6d3b1
Cairo <cairofelipedev@gmail.com>
2024-02-28 08:06:59 -0300

    Add context

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,38 +1,24 @@
 import { ApolloServer } from "@apollo/server";
 import { User } from "./user";
-import { Product } from "./product";
-import { prismaClient } from "../lib/db";
 
 async function createApolloGraphqlServer() {
   const gqlServer = new ApolloServer({
     typeDefs: `
-            ${Product.queries},
-
+            ${User.typeDefs}
             type Query {
-              ${User.queries}
-            },
+               ${User.queries}
+            }
 
             type Mutation {
-               ${User.mutations},
-               ${Product.mutations}
+               ${User.mutations}
             }
         `,
     resolvers: {
       Query: {
-        products: async () => {
-          return await prismaClient.product.findMany({
-            select: {
-              id: true,
-              name: true,
-              price: true,
-            },
-          });
-        },
         ...User.resolvers.queries,
       },
       Mutation: {
         ...User.resolvers.mutations,
-        ...Product.resolvers.mutations,
       },
     },
   });
